67cd98329316eecb1c1bff9d604b30c8c5e53424
Vitaliy <korolevva88@yandex.ru>
2020-01-24 03:16:28 +0300

    Исправляет ошибки в соответствии с комментариями

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,7 +1,7 @@
-var buttonMenu = document.querySelector("page-header__toggle");
 var menu = document.querySelector(".main-nav");
 var buttonToggle = document.querySelector(".page-header__toggle");
 
+buttonToggle.classList.toggle("page-header__toggle--hide");
 menu.classList.toggle("main-nav--close");
 
 buttonToggle.addEventListener("click", function(evt) {
